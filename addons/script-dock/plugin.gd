0231b5cb7e755076108c74948d7cfecc33f76a83
dugramen <koliur.rahman@my.liu.edu>
2024-03-08 16:18:33 -0500

    focus the scripts dock when a script is edited

--- addons/script-dock/plugin.gd
+++ addons/script-dock/plugin.gd
@@ -6,7 +6,10 @@ var panel: Control
 var default_parent: Control
 
 func _enter_tree():
-	default_parent = EditorInterface.get_script_editor().get_child(0).get_child(1)
+	var script_editor := EditorInterface.get_script_editor()
+	script_editor.editor_script_changed.connect(on_script_changed)
+	
+	default_parent = script_editor.get_child(0).get_child(1)
 	panel = default_parent.get_child(0)
 	panel.reparent(control)
 	
@@ -14,6 +17,10 @@ func _enter_tree():
 	control.size_flags_vertical = Control.SIZE_EXPAND_FILL
 	control.name = "Scripts"
 	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR, control)
+
+func on_script_changed(scr: Script):
+	var parent: TabContainer = control.get_parent()
+	parent.current_tab = control.get_index()
 
 func _exit_tree():
 	panel.reparent(default_parent)
