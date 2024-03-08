f8f98ba8b07f0bed43297a731bf4cdaa0a85c7e8
dugramen <koliur.rahman@my.liu.edu>
2024-03-08 15:28:52 -0500

    working script dock

---
+++ addons/script-dock/plugin.gd
@@ -0,0 +1,21 @@
+@tool
+extends EditorPlugin
+
+var control := MarginContainer.new()
+var panel: Control
+var default_parent: Control
+
+func _enter_tree():
+	default_parent = EditorInterface.get_script_editor().get_child(0).get_child(1)
+	panel = default_parent.get_child(0)
+	panel.reparent(control)
+	
+	control.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	control.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	control.name = "Scripts"
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR, control)
+
+func _exit_tree():
+	panel.reparent(default_parent)
+	default_parent.move_child(panel, 0)
+	remove_control_from_docks(control)
